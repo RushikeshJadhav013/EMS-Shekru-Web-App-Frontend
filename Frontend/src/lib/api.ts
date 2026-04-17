d62165f756750edf9ffb75ba9fab21c3d54f653f
Priya <priyakhadsare8@gmail.com>
2026-04-17 12:00:54 +0530

    changed url in updated developement file

--- Frontend/src/lib/api.ts
+++ Frontend/src/lib/api.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "https://testing.staffly.space      ";
+  import.meta.env.VITE_API_BASE_URL || "https://testing.staffly.space";
 
 interface EmployeeData {
   name: string;
